d9a0f9e4fc61d656e745ea426917dc1f56771d32
ugenkudupudiqbnox <ugen@qbnox.com>
2026-02-25 01:44:46 +0530

    Add Spanish and French translations

---
+++ lang/es/local_customreg.php
@@ -0,0 +1,46 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Configuración de cursos para Registro Personalizado';
+$string['pendingapproval'] = 'Su registro está pendiente de aprobación.';
+
+$string['areyouexisting'] = '¿Es usted un miembro existente?';
+$string['existingmember'] = "Soy un Empleado/Estudiante existente";
+$string['newmember'] = "No lo soy";
+$string['institutionid'] = 'ID de la institución';
+
+$string['manageusers'] = 'Gestionar Registros Personalizados';
+$string['approve'] = 'Aprobar';
+$string['deny'] = 'Denegar';
+$string['identitytype'] = 'Tipo de Identidad';
+$string['documentstatus'] = 'Estado del documento';
+$string['action'] = 'Acción';
+$string['uploaded'] = 'Subido';
+$string['notuploaded'] = 'No subido';
+$string['approved'] = 'Aprobado';
+$string['pending'] = 'Pendiente';
+$string['denied'] = 'Denegado';
+$string['customreg:manage'] = 'Gestionar registros personalizados';
+$string['userapproved'] = 'El usuario ha sido aprobado.';
+$string['userdenied'] = 'El registro de usuario ha sido denegado. El usuario ahora puede volver a subir su identificación.';
+$string['searchusers'] = 'Buscar usuarios o correo electrónico';
+$string['uploadagain'] = 'Su documento anterior no fue aceptado. Por favor, vuelva a subir una copia clara de su identificación gubernamental.';
+$string['availablecourses'] = 'Cursos disponibles para registro';
+$string['availablecourses_desc'] = 'Seleccione los cursos que los usuarios pueden elegir durante el proceso de registro (máximo 5).';
+$string['selectcourses'] = 'Seleccionar cursos';
+$string['maxcoursesreached'] = 'Puede seleccionar un máximo de 5 cursos.';
+$string['atleastonecourse'] = 'Debe seleccionar al menos un curso.';
+$string['approvecourse'] = 'Aprobar curso';
+$string['denycourse'] = 'Denegar curso';
+$string['approveallcourses'] = 'Aprobar todos los cursos';
+$string['enrollmentstatus'] = 'Estado de inscripción';
+$string['coursetitle'] = 'Título del curso';
+$string['enrollsuccess'] = 'Usuario inscrito en el curso con éxito.';
+$string['alreadyenrolled'] = 'El usuario ya está inscrito en este curso.';
+
+$string['course1'] = 'Primer curso';
+$string['course2'] = 'Segundo curso';
+$string['course3'] = 'Tercer curso';
+$string['course4'] = 'Cuarto curso';
+$string['course5'] = 'Quinto curso';
+$string['selectacourse'] = 'Seleccionar un curso';
---
+++ lang/fr/local_customreg.php
@@ -0,0 +1,46 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Paramètres de cours pour inscription personnalisée';
+$string['pendingapproval'] = 'Votre inscription est en attente d\'approbation.';
+
+$string['areyouexisting'] = 'Êtes-vous déjà membre ?';
+$string['existingmember'] = "Je suis un employé/étudiant existant";
+$string['newmember'] = "Je ne le suis pas";
+$string['institutionid'] = 'ID de l\'institution';
+
+$string['manageusers'] = 'Gérer les inscriptions personnalisées';
+$string['approve'] = 'Approuver';
+$string['deny'] = 'Refuser';
+$string['identitytype'] = 'Type d\'identité';
+$string['documentstatus'] = 'Statut du document';
+$string['action'] = 'Action';
+$string['uploaded'] = 'Téléversé';
+$string['notuploaded'] = 'Non téléversé';
+$string['approved'] = 'Approuvé';
+$string['pending'] = 'En attente';
+$string['denied'] = 'Refusé';
+$string['customreg:manage'] = 'Gérer les inscriptions personnalisées';
+$string['userapproved'] = 'L\'utilisateur a été approuvé.';
+$string['userdenied'] = 'L\'inscription de l\'utilisateur a été refusée. L\'utilisateur peut maintenant à nouveau téléverser sa pièce d\'identité.';
+$string['searchusers'] = 'Rechercher des utilisateurs ou des courriels';
+$string['uploadagain'] = 'Votre document précédent n\'a pas été accepté. Veuillez recommencer le téléversement d\'une copie claire de votre pièce d\'identité officielle.';
+$string['availablecourses'] = 'Cursos disponibles para inscripción';
+$string['availablecourses_desc'] = 'Sélectionnez les cours que les utilisateurs peuvent choisir lors du processus d\'inscription (max 5).';
+$string['selectcourses'] = 'Sélectionner les cours';
+$string['maxcoursesreached'] = 'Vous pouvez sélectionner au maximum 5 cours.';
+$string['atleastonecourse'] = 'Vous devez sélectionner au moins un cours.';
+$string['approvecourse'] = 'Approuver le cours';
+$string['denycourse'] = 'Refuser le cours';
+$string['approveallcourses'] = 'Approuver tous les cours';
+$string['enrollmentstatus'] = 'Statut d\'inscription';
+$string['coursetitle'] = 'Titre du cours';
+$string['enrollsuccess'] = 'L\'utilisateur a été inscrit au cours avec succès.';
+$string['alreadyenrolled'] = 'L\'utilisateur est déjà inscrit à ce cours.';
+
+$string['course1'] = 'Premier cours';
+$string['course2'] = 'Deuxième cours';
+$string['course3'] = 'Troisième cours';
+$string['course4'] = 'Quatrième cours';
+$string['course5'] = 'Cinquième cours';
+$string['selectacourse'] = 'Sélectionner un cours';
